cdf6d060d2fc09f665857f7a0e27af75a25687a6
Sikarwar Softwares <info@ssoftwares.in>
2022-07-15 15:16:21 +0530

    HTTP server and socket added

--- src/app.ts
+++ src/app.ts
@@ -1,35 +1,66 @@
-console.log("Hello World");
-
 import b from 'bonescript';
+import express from 'express';
+import { createServer } from "http";
+import { Server } from "socket.io";
+import path from 'path';
 
-const m = -0.243;
-const yIntercept = 0.323;
-// const Ro = 48;
-const Ro = 85;
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer);
 
-setTimeout(readValues , 1000);
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/../index.html'));
+});
+
 init();
+setTimeout(readValues, 1000);
 
-function readValues(){
+function readValues() {
+
+    const m = -0.243;
+    const yIntercept = 0.323;
+    // const Ro = 48;
+    const Ro = 85;
+
     const value = b.analogRead("A0");
     // process.stdout.write("Pin A0"  + ": " + (value*100).toFixed(1) + '%, ' + (1.8*value).toFixed(3) + 'V   \r');
-    setTimeout(readValues , 1000);
-    
-    // const Vrl = (1.8*value).toFixed(3);
-    const Vrl = (1.8*value);
+    setTimeout(readValues, 1000);
 
-    const Rs = ((5.0/Vrl) - 1) * 10;
+    // const Vrl = (1.8*value).toFixed(3);
+    const Vrl = (1.8 * value);
+
+    const Rs = ((5.0 / Vrl) - 1) * 10;
 
     // const Ro = Rs/3.6;
-    const ratio = Rs/Ro;
-    
-    
-    const PPM = Math.pow(10 , (Math.log10(ratio)-yIntercept)/m );
-    
-    console.log({Vrl , Rs , Ro , ratio , PPM});
+    const ratio = Rs / Ro;
+
+
+    const PPM = Math.pow(10, (Math.log10(ratio) - yIntercept) / m);
+
+    console.log({ Vrl, Rs, Ro, ratio, PPM });
+    io.emit("sensor", { voltage: Vrl.toFixed(3), ppm: PPM.toFixed(3)});
 }
 
-function init(){
-    console.log("Setting up socket server");
+function init() {
+    httpServer.listen(3000, () => {
+        console.log("Listening on port " + 3000);
+    });
+
+    io.on("connection", (socket) => {
+        console.log("Device Connected", socket.id);
+
+        socket.on("disconnect", (reason) => {
+            console.log("Device Disconnected");
+        });
+    });
 }
 
+// function publishValues() {
+//     io.emit("sensor", { voltage: generateRandomInteger(0, 5), ppm: generateRandomInteger(0, 20) });
+// }
+
+// function generateRandomInteger(min: number, max: number) {
+//     return Math.floor(min + Math.random() * (max - min + 1))
+// }
+
+// setInterval(publishValues, 1000);
--- src/webserver.ts
+++ src/webserver.ts
@@ -1,12 +1,18 @@
 import http from 'http';
+var fs = require('fs');            //required to readfile
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  // res.statusCode = 200;
+  // res.setHeader('Content-Type', 'text/plain');
+  // res.end('Hello World');
+  fs.readFile('index.html', function (err: any, data:any) {
+    res.writeHead(200);
+    res.write(data);
+    res.end();
+  });
 });
 
 server.listen(port, hostname, () => {
